fe65fb7037062c129807d319ce805dff48151aee
sichiiii <kirillfisyukovich@gmail.com>
2022-04-10 03:39:55 +0300

    1

---
+++ blog/views.py
@@ -0,0 +1,17 @@
+from django.views import generic
+from .models import Post, Contact
+
+
+class PostList(generic.ListView):
+    queryset = Post.objects.filter(status=1).order_by('-created_on')
+    template_name = 'index.html'
+
+
+class PostDetail(generic.DetailView):
+    model = Post
+    template_name = 'post_detail.html'
+
+
+class Contact(generic.DetailView):
+    model = Contact
+    template_name = 'contact.html'
